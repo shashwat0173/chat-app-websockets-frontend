602a68cb0f63750edb502b7681ba93ca5759865d
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-08-08 15:53:58 +0530

    feat(ui): implement real-time chat client with WebSocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const wsRef = useRef();
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    };
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "red",
+          },
+        })
+      );
+    };
+    return () => {
+      ws.close();
+    };
+  }, []);
 
   return (
-    <>
-    
-    </>
-  )
+    <div className="h-screen bg-black">
+      <br />
+      <br />
+      <br />
+      <div className="h-[85vh]">
+        {messages.map((message) => (
+          <div className="m-8">
+            <span className="bg-white text-black rounded p-4 ">{message}</span>
+          </div>
+        ))}
+      </div>
+      <div className="w-full bg-white flex">
+        <input ref={inputRef} id="message" className="flex-1 p-4"></input>
+        <button
+          onClick={() => {
+            const message = inputRef.current?.value;
+            wsRef.current.send(
+              JSON.stringify({
+                type: "chat",
+                payload: {
+                  message: message,
+                },
+              })
+            );
+          }}
+          className="bg-purple-600 text-white p-4"
+        >
+          Send message
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
